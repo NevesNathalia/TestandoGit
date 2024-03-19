f888a9dc63f0d9edc3861c07729be010bce5bf98
Nathália <nathalianevesdasilva@2005>
2024-03-19 09:48:31 -0400

    escrevi um comentário

---
+++ primeiroProj/primeiroProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  primeiroProj
+//
+//  Created by Nathalia Neves on 19/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Testando e praticando")
+    }
+
+
+}
+
